b9ce7d49a9e4f2b473ce1d27be81b2ad7bf416df
Saurabh Melge <Saurabhmelge.Melge@veritas.com>
2021-05-06 20:41:29 +0530

    Directory traversal code added.

---
+++ client.cpp
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <vector>
+#include <string>
+
+#define SIZE 1024
+
+using namespace std;
+
+int send_file(int sockfd, const char *file_name);
+
+std::vector<string> traverse_dir(string dir_name);
+
+int main() {
+	/*char server_response[SIZE] = {'\0'};
+
+	int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	struct sockaddr_in server_address;
+
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(18000);
+        server_address.sin_addr.s_addr = inet_addr("10.210.77.140");
+
+
+	int status = connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address));
+	if (status == -1) {
+		printf("Failed to connect to the server.");
+		return 1;
+	}
+
+	recv(client_socket, &server_response, sizeof(server_response), 0);
+
+	printf("Reponse from Server: %s\n", server_response);
+
+	//file transfer
+	status = send_file(client_socket, "test.txt");
+	if (status != 0) {
+		printf("Error in sending file data.");
+		return 1;
+	}
+
+	printf("File has been successfully backed up.");	
+
+	close(client_socket);
+    */
+
+	std::vector<string> final_file_list = traverse_dir(".");
+	printf("Length of filelist: [%d\n]", final_file_list.size());
+
+	return 0;
+}
+
+std::vector<string> traverse_dir(string dir_name) {
+	static std::vector<string> file_list;
+
+	DIR* dir;
+	struct dirent *ent;
+	struct stat states;
+
+	char entry_name[SIZE] = {'\0'};
+
+	dir = opendir(dir_name.c_str());
+	if (dir == NULL) {
+		printf("Failed to open the directory");
+		return file_list;
+	}
+
+	while ((ent = readdir(dir)) != NULL) {
+			stat(ent->d_name, &states);
+
+			if (!strcmp(".", ent->d_name) || !strcmp("..", ent->d_name)) {
+					continue;
+			}
+			else {
+					sprintf(entry_name, "%s/%s", dir_name.c_str(), ent->d_name);
+					
+					if (S_ISDIR(ent->d_type & DT_DIR)) {
+							traverse_dir(entry_name);
+					}
+
+					file_list.push_back(entry_name);
+					printf("%s\n", entry_name);
+			}
+	}
+
+	closedir(dir);
+
+	return file_list;
+}
+
+int send_file(int sockfd, const char *file_name) {	
+	char data[SIZE] = {'\0'};
+	int status = 0;
+
+	FILE *fp = fopen(file_name, "r");
+	if (fp == NULL) {
+		printf("Failed to open the file [%s].", file_name);
+		return 1;
+	}
+
+	while (fgets(data, SIZE, fp) != NULL) {
+		status = send(sockfd, data, sizeof(data), 0);
+		if (status == -1) {
+			return 1;
+		}
+
+		memset(data, 0, sizeof(data));
+	}
+
+	return 0;
+}
+
